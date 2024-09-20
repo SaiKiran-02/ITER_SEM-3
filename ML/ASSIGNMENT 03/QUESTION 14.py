3546288a134fc4581e379a2e9e1ca1e7bbd01cc8
SaiKiran-02 <saikiran27mohanty@gmail.com>
2024-09-20 17:32:06 +0530

    Assignments

--- ML/ASSIGNMENT 03/QUESTION 14.py
+++ ML/ASSIGNMENT 03/QUESTION 14.py
@@ -1,2 +1,19 @@
-def sin(x):
-    p = 3
+import math as m
+
+def sine(x):
+    sineVal = x
+    n = 3
+    sign = -1
+    term = 100
+
+    while(abs(term)>1e-6):
+        term = sign * (x**n)/m.factorial(n)
+        sineVal +=term
+        n +=2
+        sign *=-1
+
+    return sineVal
+
+theta = m.pi/4
+print(f"sin({theta: 0.4f}) = {sine(theta): 0.4f}")
+print("Actual value = ",m.sin(theta))
