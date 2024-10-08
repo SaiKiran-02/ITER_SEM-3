b9d6e8a7518582a4bdf6f83ca2b01d62456a9527
SaiKiran-02 <saikiran27mohanty@gmail.com>
2024-10-08 22:06:52 +0530

    Assignments

--- ML/ASSIGNMENT 06/QUESTION 06.py
+++ ML/ASSIGNMENT 06/QUESTION 06.py
@@ -3,3 +3,15 @@
 Q 06 Write function to_binary(n) that recursively converts the given positive integer into a
 textual binary representation. No call to int(x, base) may be used.
 '''
+def to_binary(n):
+    if n == 0:
+        return ""
+    else:
+        return to_binary(n//2) + str(n%2)
+
+n = 8
+if n == 0:
+    print('0')
+else:
+    print(to_binary(n))
+
