25fa57bdcc8431e0526ac7437e817e4560c6a9c3
SaiKiran-02 <saikiran27mohanty@gmail.com>
2024-10-09 10:14:59 +0530

    Assignments

--- ML/ASSIGNMENT 06/QUESTION 08.py
+++ ML/ASSIGNMENT 06/QUESTION 08.py
@@ -6,3 +6,13 @@ Q 08 Write recursive function power_of(value, exponent) that exponentiates the g
 
      2^4 = 16. You may not use the built-in functionality pow() or the operator **.
 '''
+
+def power_of(b , p):
+    if b == 1 or p == 0:
+        return 1
+    if b == 0:
+        return 0
+    else:
+        return b * power_of(b , p-1)
+
+print(power_of(2,10))
--- ML/ASSIGNMENT 06/QUESTION 09.py
+++ ML/ASSIGNMENT 06/QUESTION 09.py
@@ -5,3 +5,25 @@ Q 09 A palindrome is a word that reads the same from the front and the back. You
      is_number_palindrome(number) but without converting the number into a string and
      then using string functionalities like [::-1].
 '''
+
+
+def is_number_palindrome(number):
+    # Helper function to find the reverse of the number recursively
+    def is_palindrome_recursive(n, reverse=0):
+        if n == 0:
+            return reverse
+        else:
+            reverse = reverse * 10 + n % 10
+            return is_palindrome_recursive(n // 10, reverse)
+    if number < 0:
+        return False
+    reversed_number = is_palindrome_recursive(number)
+
+    return number == reversed_number
+
+
+print(is_number_palindrome(121))  # True
+print(is_number_palindrome(-121))  # False
+print(is_number_palindrome(10))  # False
+print(is_number_palindrome(12321))  # True
+
